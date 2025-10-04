eaa4c46c535b50e7fa010426473da99164fb218e
zarixdev <zarix@wp.pl>
2025-10-04 15:45:39 +0200

    my first repository

---
+++ collatz.py
@@ -0,0 +1,21 @@
+def collatz(number):
+    if number % 2 == 0:
+        number = number//2
+        print (number)
+        return number
+    else:
+        number = (number *3) +1
+        print (number)
+        return number
+
+try:
+    a = int(input("Podaj liczbę: "))
+
+    while a != 1:
+        a = collatz(a)
+
+except ValueError:
+    print ("Podaj prawidłową liczbę")
+
+
+
